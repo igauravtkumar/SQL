b1aea08102c1a4a1c68f61f7f05a7b283b24fb36
Gaurav Tailor <gauravtailor185@gmail.com>
2025-03-30 20:58:23 +0530

    Add files via upload

---
+++ insert query in pl.sql
@@ -0,0 +1,16 @@
+create table test(id int);
+
+insert into test values(10);
+
+insert into test values(20);
+
+insert into test values(30);
+
+insert into test values(40);
+
+insert into test values(30);
+
+insert into test values(50);
+
+
+select * from test;
